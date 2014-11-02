ef554014dab1a85facd5b10c62e9dc4cafc55e25
Konni1337 <konni-boehm@web.de>
2014-11-02 13:01:24 +0100

    added equals to player

--- src/Core/Player.java
+++ src/Core/Player.java
@@ -3,6 +3,28 @@ package Core;
 
 public class Player extends PlayboardObject{
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Player other = (Player) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
 	private int id;
 	private boolean isAlive = true;
 	
